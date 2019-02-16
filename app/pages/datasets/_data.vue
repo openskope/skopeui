4a10d8089c9e828a9b04105f522f89565ce7cf8b
Christine Nguyen <Christine.N.Nguyen@asu.edu>
2019-02-15 17:09:31 -0700

    initialized implementation of Vuex store to manage state for datasets created dynamic vue route component

---
+++ app/pages/datasets/_data.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <article>
+      <h1>This is a title for a given dataset</h1>
+      <p>This is a description for a given dataset</p>
+    </article>
+
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    dataset() {
+      // accesses store, navigates to state, goes to datasets module, get all datasets
+      // that live in "all" property
+      return this.$store.state.datasets.all.find(
+        dataset => dataset.title === this.title
+      )
+    }
+  }
+}
+</script>
