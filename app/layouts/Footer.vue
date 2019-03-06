2cb6bf35fe6262575015c2cbce6a62b57df2e028
chrstngyn <christine@christine-xps15.3500.dhcp.asu.edu>
2019-03-06 13:31:53 -0700

    moved footer from layouts/default.vue to its own layout to modularize UI created function in Footer to return current year

---
+++ app/layouts/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-footer
+    light
+    height="auto"
+    app
+    absolute
+  >
+    <v-card flat tile class="flex indigo lighten-1 white--text text-xs-center">
+      <v-card-text>
+        <v-btn v-for="icon in icons" :key="icon" class="mx-3 white--text" icon>
+          <v-icon size="24px">
+            {{ icon }}
+          </v-icon>
+        </v-btn>
+      </v-card-text>
+      <v-divider />
+      <v-card-text class="white--text">
+        <div>&copy; 2016 - {{ year }}</div>
+      </v-card-text>
+    </v-card>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  data() {
+    return {
+      year: new Date().getFullYear()
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
