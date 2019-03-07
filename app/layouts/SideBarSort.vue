928046cdbe1484c82863154246c378420fb398ad
Allen Lee <allen.lee@asu.edu>
2019-03-07 15:32:52 -0700

    lint and update yarn.lock file i
    - also binding hapi to 0.0.0.0 for the time being

--- app/layouts/SideBarSort.vue
+++ app/layouts/SideBarSort.vue
@@ -1,7 +1,5 @@
 <template>
-  <v-list>
-
-  </v-list>
+  <v-list />
 </template>
 <script>
 export default {
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -120,25 +120,25 @@
       </v-container>
     </v-content>
     <v-footer
-    light
-    height="auto"
-    app
-    absolute
-  >
-    <v-card flat tile class="flex indigo lighten-1 white--text text-xs-center">
-      <v-card-text>
-        <v-btn v-for="icon in icons" :key="icon" class="mx-3 white--text" icon>
-          <v-icon size="24px">
-            {{ icon }}
-          </v-icon>
-        </v-btn>
-      </v-card-text>
-      <v-divider />
-      <v-card-text class="white--text">
-        <div>&copy; 2016 - {{ new Date().getFullYear() }}</div>
-      </v-card-text>
-    </v-card>
-  </v-footer>
+      light
+      height="auto"
+      app
+      absolute
+    >
+      <v-card flat tile class="flex indigo lighten-1 white--text text-xs-center">
+        <v-card-text>
+          <v-btn v-for="icon in icons" :key="icon" class="mx-3 white--text" icon>
+            <v-icon size="24px">
+              {{ icon }}
+            </v-icon>
+          </v-btn>
+        </v-card-text>
+        <v-divider />
+        <v-card-text class="white--text">
+          <div>&copy; 2016 - {{ new Date().getFullYear() }}</div>
+        </v-card-text>
+      </v-card>
+    </v-footer>
   </v-app>
 </template>
 
