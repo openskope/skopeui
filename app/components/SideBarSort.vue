2e095dfb2c006a00c9689ace9f04307b5618863b
chrstngyn <Christine.N.Nguyen@asu.edu>
2019-03-08 16:50:50 -0700

    changed ul to v-list in SideBarSort for variable checkbox list

--- app/components/SideBarSort.vue
+++ app/components/SideBarSort.vue
@@ -1,6 +1,39 @@
 <template>
   <div class="container">
-    <ul
+    <v-layout row>
+      <v-flex
+        shrink
+        style="width: 60px"
+      >
+        <v-text-field
+          v-model="bounds[0]"
+          class="mt-0"
+          hide-details
+          single-line
+          type="number"
+        />
+      </v-flex>
+      <v-flex class="px-3">
+        <v-range-slider
+          v-model="bounds"
+          :max="2019"
+          :min="1"
+          :step="1"
+        />
+      </v-flex>
+      <v-flex>
+        <v-text-field
+          v-model="bounds[1]"
+          class="mt-0"
+          hide-details
+          single-line
+          type="number"
+        />
+      </v-flex>
+    </v-layout>
+    <v-list
+      dense
+      :class="{fix: fix, closer: closer}"
       v-for="variable in variables"
       :key="variable">
       <v-checkbox
@@ -8,10 +41,12 @@
         value="variable.name"
         :label="variable.name"
       />
-    </ul>
+    </v-list>
+    <!-- spacing between sorting methods -->
     <v-spacer />
     <v-divider />
     <v-spacer />
+    <!-- end spacing between sorting methods -->
     <v-radio-group v-model="radioSelect">
       <v-radio
         v-for="i in 5"
@@ -20,9 +55,11 @@
         :value="i"
       />
     </v-radio-group>
+    <!-- spacing between sorting methods -->
     <v-spacer />
     <v-divider />
     <v-spacer />
+    <!-- end spacing between sorting methods -->
   </div>
 </template>
 <script>
@@ -30,6 +67,7 @@ export default {
   name: 'SideBarSort',
   data() {
     return {
+      bounds: [1, 2019],
       variables: [
         { name: 'variable1', checked: false },
         { name: 'variable2', checked: false },
