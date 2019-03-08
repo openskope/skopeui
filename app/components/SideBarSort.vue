a570c4a377238cdfa387899b85ddc5d6487838fb
chrstngyn <Christine.N.Nguyen@asu.edu>
2019-03-08 16:40:47 -0700

    radio button list added to SideBarSort component. selection of radio buttons works without returning error

--- app/components/SideBarSort.vue
+++ app/components/SideBarSort.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="container">
     <ul
       v-for="variable in variables"
       :key="variable">
@@ -9,6 +9,20 @@
         :label="variable.name"
       />
     </ul>
+    <v-spacer />
+    <v-divider />
+    <v-spacer />
+    <v-radio-group v-model="radioSelect">
+      <v-radio
+        v-for="i in 5"
+        :key="i"
+        :label="`Radio ${ i } `"
+        :value="i"
+      />
+    </v-radio-group>
+    <v-spacer />
+    <v-divider />
+    <v-spacer />
   </div>
 </template>
 <script>
@@ -20,7 +34,8 @@ export default {
         { name: 'variable1', checked: false },
         { name: 'variable2', checked: false },
         { name: 'variable3', checked: false }
-      ]
+      ],
+      radioSelect: 1
     }
   }
 }
