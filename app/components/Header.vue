63065dc4919eda1ef1e1b2b2d2478555a8dd61e5
chrstngyn <Christine.N.Nguyen@asu.edu>
2019-03-29 09:15:30 -0700

    header navigation removed. primary navigation shifted to stepper navigation

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -10,14 +10,6 @@
       </nuxt-link>
     </v-toolbar-title>
     <v-spacer />
-    <v-toolbar-items class="hidden-sm-and-down">
-      <v-btn flat to="/">
-        Datasets
-      </v-btn>
-      <v-btn flat to="/map">
-        Interactive Map
-      </v-btn>
-    </v-toolbar-items>
   </v-toolbar>
 </template>
 <script>
