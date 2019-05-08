0d3f652295fe591d7d96aa2a60eec0d6c4edd17f
chrstngyn <Christine.N.Nguyen@asu.edu>
2019-05-08 15:30:25 -0700

    removed search bar component and moved to sidebarsort

--- app/components/SideBar.vue
+++ app/components/SideBar.vue
@@ -4,27 +4,6 @@
     app
   >
     <section class="sidebar">
-      <!-- search form -->
-      <form id="searchForm" class="sidebar-form" @submit.prevent>
-        <div id="searchContainer" class="input-group">
-          <span class="input-group-btn">
-            <input
-              id="search"
-              type="text"
-              name="search"
-              class="search form-control"
-              data-toggle="hideseek"
-              placeholder="Search datatsets"
-              data-list=".sidebar-menu"
-            >
-            <button id="search-btn" type="submit" name="search" class="btn btn-flat">
-              <i class="fa fa-search" />
-            </button>
-          </span>
-        </div>
-      </form>
-      <!-- end search form -->
-      <v-spacer />
       <!-- sidebar sort -->
       <side-bar-sort />
       <!-- end sidebar sort -->
@@ -49,25 +28,6 @@ export default {
 </script>
 
 <style scoped>
-#searchForm {
-  padding-left: 0em;
-  padding-right: 0em;
-}
-
-#searchContainer {
-  height: 100%;
-  padding-bottom: 0em;
-}
-
-#search {
-  width: 80%;
-  float: right;
-}
-
-#search-btn {
-  width: 20%;
-}
-
 .sidebar {
   padding-top: 30px;
 }
