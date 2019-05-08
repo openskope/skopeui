2f3cb3684182be521107b041b308483c21342906
chrstngyn <Christine.N.Nguyen@asu.edu>
2019-05-08 15:32:40 -0700

    started implementing code to generate variable selection via store started writing methods to filter datasets moved search bar to this vue component todo: fix store - variables not generating properly

--- app/components/SideBarSort.vue
+++ app/components/SideBarSort.vue
@@ -1,5 +1,30 @@
 <template>
   <div class="container">
+    <!-- search form -->
+    <form id="searchForm" class="sidebar-form" @submit.prevent>
+      <div id="searchContainer" class="input-group">
+        <span class="input-group-btn">
+          <input
+            id="search"
+            type="text"
+            name="search"
+            class="search form-control"
+            data-toggle="hideseek"
+            placeholder="Search datatsets"
+            data-list=".sidebar-menu"
+            @keydown.enter="search"
+          >
+          <button id="search-btn" type="submit" name="search" class="btn btn-flat" @click="search">
+            <i class="fa fa-search" />
+          </button>
+        </span>
+      </div>
+    </form>
+    <!-- end search form -->
+    <v-spacer />
+    <v-divider />
+    <v-spacer />
+    <!-- time range slider -->
     <v-subheader>Range Slider</v-subheader>
     <v-layout row>
       <v-flex
@@ -32,19 +57,27 @@
         />
       </v-flex>
     </v-layout>
+    <!-- end time range slider -->
     <v-spacer />
+    <v-divider />
+    <v-spacer />
+
+    <!-- variable checkbox selector -->
     <v-subheader>Variables</v-subheader>
     <v-list
-      v-for="variable of variables"
-      :key="variable.name"
+      v-for="(variable, index) in variables"
+      :key="index"
       dense
     >
       <v-checkbox
         v-model="variable.checked"
         value="variable.name"
         :label="variable.name"
+        :change="getFilteredDatasets"
       />
     </v-list>
+    <!-- end variable checkbox selector -->
+
     <!-- spacing between sorting methods -->
     <v-spacer />
     <v-divider />
@@ -71,16 +104,81 @@ export default {
   name: 'SideBarSort',
   data() {
     return {
+      filteredDatasets: [],
+      filteredByVariables: [],
+      datasets: [],
+      search: '',
       bounds: [1, 2019],
       variables: [
+        // populate this programmatically
         { name: 'variable1', checked: false },
         { name: 'variable2', checked: false },
         { name: 'variable3', checked: false }
       ],
       radioSelect: 1
     }
+  },
+
+  computed: {
+    selectedVariableFilters() {
+      let checkedVariableFilters = []
+      const variableFilters = []
+      checkedVariableFilters = this.variables.filter(obj => obj.checked)
+      checkedVariableFilters.forEach(element => {
+        variableFilters.push(element.name)
+      })
+      return variableFilters
+    },
+    defaultDataset() {
+      return this.$store.datasets
+    }
+  },
+  mounted() {
+    this.setVariablesArray()
+  },
+  methods: {
+    getFilteredDatasets() {
+      // filter checked variable filters
+      if (this.selectedVariableFilters.length > 0) {
+        this.filteredByVariables = this.filteredDatasets.filter(obj =>
+          this.selectedVariableFilters.every(
+            val => obj.variable.indexOf(val) >= 0
+          )
+        )
+        this.filteredDatasets = this.filteredByVariables
+      }
+    },
+    setVariablesArray() {
+      this.variables = []
+      this.datasets = this.defaultDataset
+      for (let i = 0; i < this.datasets.length; i++) {
+        this.variables.push({
+          name: this.datasets.variables.name,
+          checked: false
+        })
+      }
+      return this.variables
+    }
   }
 }
 </script>
 <style scoped>
+#searchForm {
+  padding-left: 0em;
+  padding-right: 0em;
+}
+
+#searchContainer {
+  height: 100%;
+  padding-bottom: 15px;
+}
+
+#search {
+  width: 80%;
+  float: right;
+}
+
+#search-btn {
+  width: 20%;
+}
 </style>
