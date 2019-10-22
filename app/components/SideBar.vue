86988bd37692f70cc6a85c45f8e2eb45c2630668
Calvin Pritchard <pritchard.calvin@gmail.com>
2019-10-22 16:50:20 -0700

    Added 'position: fixed' as workaround for nav-drawer

--- app/components/SideBar.vue
+++ app/components/SideBar.vue
@@ -1,5 +1,6 @@
 <template>
-  <v-navigation-drawer absolute app>
+  <v-navigation-drawer absolute app style="position: fixed">
+    <!-- had to fixed style since fixed prop didn't work -->
     <section class="sidebar">
       <discover-side-bar />
     </section>
