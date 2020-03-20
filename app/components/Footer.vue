453d12fd6fefc39da59f366aa85ae06c959c8530
Allen Lee <allen.lee@asu.edu>
2020-03-19 22:25:34 -0700

    add NSF acknowledgements to footer

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -7,19 +7,24 @@
       tile
       class="indigo lighten-1 mx-auto white--text text-xs-center"
     >
-      <v-card-text>
-        <v-btn icon :href="github.url">
-          <v-icon size="24px" class="mx-3 white--text">{{
-            github.icon
-          }}</v-icon>
-        </v-btn>
-        <v-btn text class="white--text" :href="skope.url">
-          Skope
-        </v-btn>
+      <v-card-text class="white--text">
+        <div>
+          This material is based upon work supported by the National Science
+          Foundation under Grant Nos. SMA 1439591 and 1637189 to Arizona State
+          University, SMA 1439603 and 1637155 to the University of Illinois at
+          Urbana-Champaign, and SMA 1439516 and 1637171 to Washington State
+          University.
+        </div>
       </v-card-text>
       <v-divider />
-      <v-card-text class="white--text">
-        <div>&copy; 2016 - {{ year }}</div>
+      <v-card-text class="white--text text-center">
+        <v-btn class="white--text" icon :href="github.url">
+          <v-icon size="24px" class="mx-3">{{ github.icon }}</v-icon>
+        </v-btn>
+        <v-btn class="white--text" text :href="skope.url">
+          SKOPE
+        </v-btn>
+        &copy; 2016 - {{ year }}
       </v-card-text>
     </v-card>
   </v-footer>
@@ -43,5 +48,3 @@ export default {
   }
 }
 </script>
-
-<style scoped></style>
