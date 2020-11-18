b84cdf1bf7102e377363a3e1faa91a0c8ef1c09e
chrstngyn <Christine.N.Nguyen@asu.edu>
2020-11-18 15:57:19 -0700

    refactor: create global component dir
    move Header and Footer components into global dir

--- app/components/global/Header.vue
+++ app/components/global/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app-bar src="/header.png" shrink-on-scroll dense app>
+  <v-app-bar src="/header.png" shrink-on-scroll app>
     <v-toolbar-title>
       <nuxt-link class="skope-title" to="/">
         <v-img
