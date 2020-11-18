6df1331319a34f8b2cabdb96fa8a4b7d3981d6f7
chrstngyn <Christine.N.Nguyen@asu.edu>
2020-11-18 16:01:17 -0700

    feat: create navigation component
    create skeleton for top nav stepper component:
    Select Dataset <-> Define Study Area <-> Animate Map <-> Visualize Data <-> View Metadata

---
+++ app/components/global/Navigation.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-stepper>
+    <v-stepper-header>
+      <v-stepper-step complete editable step="1"
+        >Select Data Set</v-stepper-step
+      >
+      <v-divider></v-divider>
+      <v-stepper-step complete step="2">Define Study Area</v-stepper-step>
+      <v-divider></v-divider>
+      <v-stepper-step step="3" editable>Animate Map</v-stepper-step>
+      <v-divider></v-divider>
+      <v-stepper-step step="4" editable>Visualize Data</v-stepper-step>
+      <v-divider></v-divider>
+      <v-stepper-step step="3" editable>View Metadata</v-stepper-step>
+    </v-stepper-header>
+  </v-stepper>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+}
+</script>
+
+<style scoped></style>
