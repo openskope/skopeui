226a205a49d2f5becfad5bd70165047fe224b238
chrstngyn <Christine.N.Nguyen@asu.edu>
2020-12-17 22:41:22 -0700

    feat: generate metadata on metadata page

---
+++ app/components/action/Metadata.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-row>
+    <div v-for="(label, attr) in metadataAttributes" :key="attr" class="py-0">
+      <span class="font-weight-bold"> {{ label }}: </span>
+      <div v-html="$md.render(selectedDataset[attr])"></div>
+    </div>
+  </v-row>
+</template>
+
+<script>
+import Vue from 'vue'
+import { Component } from 'nuxt-property-decorator'
+
+@Component({
+  name: 'Metadata',
+})
+class Metadata extends Vue {
+  get selectedDataset() {
+    return this.$api().datasets.selectedDataset
+  }
+
+  get metadataAttributes() {
+    return {
+      originator: 'Originator',
+      uncertainty: 'Uncertainty',
+      methodSummary: 'Method Summary',
+      references: 'References',
+      contactInformation: 'Contact Information',
+    }
+  }
+}
+
+export default Metadata
+</script>
+
+<style scoped></style>
--- app/pages/dataset/_id/_action/index.vue
+++ app/pages/dataset/_id/_action/index.vue
@@ -1,20 +1,28 @@
 <template>
   <v-row>
-    <h1>This is a page for the action</h1>
-    <h2>An action can be study / visualize / analyze / metadata</h2>
+    <h1>action</h1>
+    <Metadata v-if="isMetadata($route.params.action)" />
   </v-row>
 </template>
 
 <script>
 import Vue from 'vue'
 import { Component } from 'nuxt-property-decorator'
+import Metadata from '@/components/action/Metadata.vue'
 
 @Component({
   layout: 'BaseDataset',
+  components: {
+    Metadata,
+  },
 })
 class Action extends Vue {
   async created() {
     console.log(this.$route.params.action)
+  }
+
+  isMetadata(slug) {
+    return slug == 'metadata'
   }
 }
 export default Action
