7aca25c2b5fe8967c7458b8fef71ceb41e93d29d
Calvin Pritchard <pritchard.calvin@gmail.com>
2021-01-15 14:28:27 -0700

    feat: added basic analyze page layout

--- app/pages/dataset/_id/analyze.vue
+++ app/pages/dataset/_id/analyze.vue
@@ -1,22 +1,68 @@
 <template>
-  <v-row>
-    <h1>Export a time series</h1>
-  </v-row>
+  <v-container>
+    <v-row class="my-5">
+      <h2 class="mx-3">
+        {{ selectedDataset.title }}
+      </h2>
+    </v-row>
+    <v-row dense align-content-start justify-space-around wrap>
+      <v-col cols="9">
+        <TimeSeriesPlot class="timeseries-flex" :time-series="timeSeries" />
+      </v-col>
+      <v-col cols="3">
+        <v-card flat outlined>
+          <v-card-title class="secondary">Series</v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import TimeSeriesPlot from '@/components/TimeSeriesPlot.vue'
 import Vue from 'vue'
 import { Component } from 'nuxt-property-decorator'
+import { namespace } from 'vuex-class'
 
+const Datasets = namespace('datasets')
 @Component({
   layout: 'BaseDataset',
+  components: {
+    TimeSeriesPlot,
+  },
 })
-class Export extends Vue {
+class Analyze extends Vue {
+  @Datasets.State('selectedDataset')
+  selectedDataset
+
+  get timeSeries() {
+    const timeseries = this.$api().dataset.timeseries
+    if (timeseries.x.length > 0) {
+      return { ...this.$api().dataset.timeseries, type: 'scatter' }
+    } else {
+      return { x: [], y: [], type: 'scatter' }
+    }
+  }
+
   async created() {
     console.log(this.$route.params.action)
   }
 }
-export default Export
+export default Analyze
 </script>
 
-<style scoped></style>
+<style scoped>
+.timeseries-flex {
+  height: calc(70vh - 96px);
+}
+@media all and (max-width: 960px) {
+  .timeseries-flex {
+    height: 400px;
+  }
+}
+@media all and (max-width: 600px) {
+  .timeseries-flex {
+    height: 350px;
+  }
+}
+</style>
