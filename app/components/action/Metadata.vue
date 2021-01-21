0eaaca15f1a227f6a8fc81a9a22761da51a22fbd
chrstngyn <Christine.N.Nguyen@asu.edu>
2021-01-21 10:03:30 -0700

    refactor: display variables in view metadata modal

--- app/components/action/Metadata.vue
+++ app/components/action/Metadata.vue
@@ -1,5 +1,24 @@
 <template>
   <v-container fill-width fluid>
+    <v-row class="mb-4">
+      <v-list dense flat>
+        <v-list-item-title><strong>Variables:</strong></v-list-item-title>
+        <v-list-item
+          v-for="(variable, index) in selectedDataset.variables"
+          :key="index"
+        >
+          <v-list-item-content>
+            <v-list-item-title>
+              <v-chip small label color="secondary" text-color="black">
+                <v-icon>view_column</v-icon>
+                {{ variable.class }}
+              </v-chip>
+              {{ variable.name }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-row>
     <v-row>
       <div v-for="(label, attr) in metadataAttributes" :key="attr" class="py-0">
         <span class="font-weight-bold"> {{ label }}: </span>
