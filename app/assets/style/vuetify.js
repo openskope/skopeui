387d7fb733fa0b43bb46686a88ffd1c79b602bd6
chrstngyn <Christine.N.Nguyen@asu.edu>
2021-02-03 11:47:01 -0700

    style: refine theme styles/variables

--- app/assets/style/vuetify.js
+++ app/assets/style/vuetify.js
@@ -1,9 +1,12 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
-  primary: '#2A324B',
-  accent: '#AC3931',
-  secondary: '#8BBF9F',
+  primary: '#49be83',
+  accent: '#fb7a2c',
+  secondary: '#09acfe',
+  // primary: '#49be83',
+  // accent: '#fb7a2c',
+  // secondary: '#09acfe',
   info: '#767B91',
   warning: '#F9C80E',
   error: '#AD1F1F',
@@ -11,18 +14,18 @@ export default {
   theme: {
     themes: {
       light: {
-        primary: '#2A324B',
-        accent: '#AC3931',
-        secondary: '#8BBF9F',
+        primary: '#49be83',
+        accent: '#fb7a2c',
+        secondary: '#09acfe',
         info: '#767B91',
         warning: '#F9C80E',
         error: '#AD1F1F',
         success: '#B5B682',
       },
       dark: {
-        primary: '#2A324B',
-        accent: '#AC3931',
-        secondary: '#8BBF9F',
+        primary: '#49be83',
+        accent: '#fb7a2c',
+        secondary: '#09acfe',
         info: '#767B91',
         warning: '#F9C80E',
         error: '#AD1F1F',
