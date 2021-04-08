59656897cc33b3b5a4b824f0b06e548d22c3a417
chrstngyn <Christine.N.Nguyen@asu.edu>
2021-04-07 17:14:21 -0700

    refactor: move export/import geo json into map component
    move toolbar into map component
    refactor references geometry -> geo json

--- app/components/Map.vue
+++ app/components/Map.vue
@@ -1,55 +1,87 @@
 <template>
-  <v-row class="fill-height">
-    <client-only placeholder="Loading map, please wait...">
-      <l-map
-        ref="layerMap"
-        :min-zoom="3"
-        :zoom="4"
-        :center="metadata.region.center"
-        @ready="mapReady"
-      >
-        <l-control-attribution v-if="showMapControls" position="topright" />
-        <l-tile-layer
-          v-for="provider of leafletProviders"
-          :key="provider.name"
-          :url="provider.url"
-          :name="provider.name"
-          :attribution="provider.attribution"
-          :visible="provider.default"
-          :min-zoom="provider.minZoom"
-          :max-zoom="provider.maxZoom"
-          layer-type="base"
-        />
-        <l-rectangle
-          :bounds="metadata.region.extents"
-          :style="metadata.region.style"
-          :fill-opacity="defaultRegionOpacity"
-        />
-        <l-control-layers
-          v-if="showMapControls"
-          :sort-layers="false"
-          position="topright"
-        />
-        <l-wms-tile-layer
-          v-for="v of metadata.variables"
-          ref="wmsLayers"
-          :key="v.id"
-          :base-url="skopeWmsUrl"
-          :layers="fillTemplateYear(v.wmsLayer)"
-          :name="v.name"
-          :crs="defaultCrs"
-          :transparent="true"
-          :opacity="layerOpacity"
-          layer-type="overlay"
-          :attribution="v.name"
-          :visible="v.visible"
-          version="1.3.0"
-          format="image/png"
-        />
-        <l-control-scale position="bottomright" />
-      </l-map>
-    </client-only>
-  </v-row>
+  <v-card class="mx-10" height="100%" elevation="2" style="z-index: 1" outlined>
+    <v-card-title>
+      <v-tooltip top>
+        <template #activator="{ on, attrs }">
+          <v-btn v-bind="attrs" text v-on="on" @click="exportSelectedGeometry">
+            <a id="exportSelectedGeometry">
+              <v-icon>fas fa-download</v-icon>
+              Download study area as a GeoJSON file
+            </a>
+          </v-btn>
+        </template>
+        <span>Download study area as a GeoJSON file</span>
+      </v-tooltip>
+      <input
+        id="loadGeoJsonFile"
+        type="file"
+        style="display: none"
+        @change="loadGeoJson"
+      />
+      <v-tooltip top>
+        <template #activator="{ on, attrs }">
+          <v-btn v-bind="attrs" text v-on="on" @click="selectGeoJsonFile">
+            <v-icon>fas fa-upload</v-icon>
+            Upload a GeoJSON file
+          </v-btn>
+        </template>
+        <span>Upload a GeoJSON file</span>
+      </v-tooltip>
+      <v-spacer></v-spacer>
+      <h3 class="headline">Selected area: {{ selectedArea }} km<sup>2</sup></h3>
+    </v-card-title>
+    <v-card-text style="height: 90%">
+      <client-only placeholder="Loading map, please wait...">
+        <l-map
+          ref="layerMap"
+          :min-zoom="3"
+          :zoom="4"
+          :center="metadata.region.center"
+          @ready="mapReady"
+        >
+          <l-control-attribution v-if="showMapControls" position="topright" />
+          <l-tile-layer
+            v-for="provider of leafletProviders"
+            :key="provider.name"
+            :url="provider.url"
+            :name="provider.name"
+            :attribution="provider.attribution"
+            :visible="provider.default"
+            :min-zoom="provider.minZoom"
+            :max-zoom="provider.maxZoom"
+            layer-type="base"
+          />
+          <l-rectangle
+            :bounds="metadata.region.extents"
+            :style="metadata.region.style"
+            :fill-opacity="defaultRegionOpacity"
+          />
+          <l-control-layers
+            v-if="showMapControls"
+            :sort-layers="false"
+            position="topright"
+          />
+          <l-wms-tile-layer
+            v-for="v of metadata.variables"
+            ref="wmsLayers"
+            :key="v.id"
+            :base-url="skopeWmsUrl"
+            :layers="fillTemplateYear(v.wmsLayer)"
+            :name="v.name"
+            :crs="defaultCrs"
+            :transparent="true"
+            :opacity="layerOpacity"
+            layer-type="overlay"
+            :attribution="v.name"
+            :visible="v.visible"
+            version="1.3.0"
+            format="image/png"
+          />
+          <l-control-scale position="bottomright" />
+        </l-map>
+      </client-only>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
@@ -62,6 +94,7 @@ import {
 } from '@/store/modules/constants'
 import circleToPolygon from 'circle-to-polygon'
 import { stringify } from 'query-string'
+import { initializeDatasetGeoJson, saveGeoJson } from '@/store/actions'
 
 const fillTemplate = require('es6-dynamic-template')
 import _ from 'lodash'
@@ -83,7 +116,6 @@ class Map extends Vue {
   legendImage = null
   legendControl = null
   legendPosition = 'bottomleft'
-  wGeometryKey = 'skope:geometry'
   wMinTemporalRangeKey = 'skope:temporal-range-min'
   wMaxTemporalRangeKey = 'skope:temporal-range-max'
 
@@ -107,8 +139,14 @@ class Map extends Vue {
     return this.$api().dataset.variable
   }
 
-  get geometry() {
-    return this.$api().dataset.geometry
+  get geoJson() {
+    return this.$api().dataset.geoJson
+  }
+
+  get selectedArea() {
+    return (this.$api().dataset.selectedAreaInSquareMeters / 1000000.0).toFixed(
+      2
+    )
   }
 
   get currentStep() {
@@ -154,6 +192,35 @@ class Map extends Vue {
     this.$api().dataset.setVariable(id)
   }
 
+  mapReady(map) {
+    const handler = (event) => {
+      console.log('handling layer change event ', { event })
+      const leafletLayer = event.layer
+      if (this.isSkopeLayer(leafletLayer)) {
+        const variable = _.find(
+          this.metadata.variables,
+          (v) => v.name === event.name
+        )
+        this.$api().dataset.setVariable(variable.id)
+        this.updateWmsLegend(map, leafletLayer.wmsParams.layers)
+        leafletLayer.bringToFront()
+      }
+    }
+    map.on('overlayadd', handler)
+    map.on('baselayerchange', handler)
+    this.addDrawToolbar(map)
+    initializeDatasetGeoJson(this.$warehouse, this.$api())
+    this.registerToolbarHandlers(map)
+  }
+
+  @Watch('geoJson') updateGeometry(geoJson) {
+    if (geoJson === null) {
+      this.drawnItems.clearLayers()
+    } else {
+      this.renderSelectedArea(geoJson)
+    }
+  }
+
   addDrawToolbar(map) {
     const L = this.$L
     this.drawnItems = new L.FeatureGroup()
@@ -190,40 +257,83 @@ class Map extends Vue {
     editControlButtons.editDisabled = 'No spatial selection to edit'
     editControlButtons.remove = 'Clear spatial selection'
     editControlButtons.removeDisabled = 'No spatial selection to remove'
-    const self = this
     map.addControl(this.drawControlFull)
+  }
+
+  registerToolbarHandlers(map) {
+    const L = this.$L
     // check for persisted geometry
-    this.checkAndRestoreSavedGeometry(map)
-    map.on(L.Draw.Event.EDITRESIZE, (event) =>
-      self.updateSelectedGeometry(event.layer)
-    )
-    map.on(L.Draw.Event.EDITMOVE, (event) =>
-      self.updateSelectedGeometry(event.layer)
-    )
-    map.on(L.Draw.Event.EDITVERTEX, (e) => self.updateSelectedGeometry(e.poly))
+    map.on(L.Draw.Event.EDITRESIZE, (event) => this.saveGeoJson(event.layer))
+    map.on(L.Draw.Event.EDITMOVE, (event) => this.saveGeoJson(event.layer))
+    map.on(L.Draw.Event.EDITVERTEX, (e) => this.saveGeoJson(e.poly))
     map.on(L.Draw.Event.CREATED, (event) => {
       const layer = event.layer
-      self.updateSelectedGeometry(layer)
-      self.drawnItems.addLayer(layer)
-      self.enableEditOnly(map)
+      this.saveGeoJson(layer)
+      this.drawnItems.addLayer(layer)
+      this.enableEditOnly(map)
     })
     map.on(L.Draw.Event.DELETED, (event) => {
-      self.clearSelectedGeometry()
-      if (self.drawnItems.getLayers().length === 0) {
-        self.disableEditOnly(map)
+      this.clearSelectedGeometry()
+      if (this.drawnItems.getLayers().length === 0) {
+        this.disableEditOnly(map)
       }
     })
   }
 
-  checkAndRestoreSavedGeometry(map) {
-    const savedGeometry = this.getSavedGeometry()
-    if (savedGeometry) {
-      this.restoreSelectedGeometry(savedGeometry, map)
-    }
-  }
-
   clearSelectedGeometry() {
     this.$api().dataset.clearGeometry()
+  }
+
+  renderSelectedArea(geoJson, map) {
+    const L = this.$L
+    const geoJsonLayer = this.toLeafletLayer(geoJson)
+    if (!map) {
+      map = this.$refs.layerMap.mapObject
+    }
+    // remove all existing layers from the FeatureGroup
+    this.drawnItems.clearLayers()
+    geoJsonLayer.eachLayer((l) => {
+      this.drawnItems.addLayer(l)
+    })
+    this.enableEditOnly(map)
+    let padding = [5, 5]
+    if (geoJsonLayer instanceof L.Marker) {
+      padding = [30, 30]
+    }
+    map.fitBounds(this.drawnItems.getBounds(), { padding })
+  }
+
+  /**
+   * persists geo json to the vuex store and local storage / warehouse and
+   * converts leaflet circles to polygons
+   */
+  saveGeoJson(layer) {
+    const data = layer.toGeoJSON()
+    if (layer instanceof L.Circle) {
+      data.properties.radius = layer.getRadius()
+      const geometry = circleToPolygon(
+        data.geometry.coordinates,
+        layer.getRadius(),
+        this.defaultCircleToPolygonEdges
+      )
+      data.geometry = geometry
+    }
+    // save geoJSON in local storage (consider pushing into the vuex store)
+    saveGeoJson(this.$warehouse, this.$api(), data)
+  }
+
+  toLeafletLayer(geoJson) {
+    const L = this.$L
+    return L.geoJson(geoJson, {
+      // convert persisted circles to L.Circle instead of L.Marker
+      pointToLayer: (feature, latlng) => {
+        if (feature.properties.radius) {
+          return new L.Circle(latlng, feature.properties.radius)
+        } else {
+          return new L.Marker(latlng)
+        }
+      },
+    })
   }
 
   disableEditOnly(map) {
@@ -251,67 +361,6 @@ class Map extends Vue {
     const queryString = stringify(query)
     const legendUrl = this.skopeWmsUrl + queryString
     return legendUrl
-  }
-
-  restoreSelectedGeometry(savedGeometry, map) {
-    const L = this.$L
-    if (!map) {
-      map = this.$refs.layerMap.mapObject
-    }
-    const geoJsonLayer = L.geoJson(savedGeometry, {
-      pointToLayer: (feature, latlng) => {
-        if (feature.properties.radius) {
-          return new L.Circle(latlng, feature.properties.radius)
-        } else {
-          return new L.Marker(latlng)
-        }
-      },
-    })
-    // remove all existing layers from the FeatureGroup
-    this.drawnItems.clearLayers()
-    geoJsonLayer.eachLayer((l) => {
-      this.drawnItems.addLayer(l)
-      this.updateSelectedGeometry(l)
-    })
-    this.enableEditOnly(map)
-    let padding = [5, 5]
-    console.log({ geoJsonLayer })
-    if (geoJsonLayer instanceof L.Marker) {
-      console.log('Setting padding for marker')
-      padding = [30, 30]
-    }
-    map.fitBounds(this.drawnItems.getBounds(), { padding })
-  }
-
-  saveSelectedGeometry(geoJson) {
-    this.$warehouse.set(this.wGeometryKey, JSON.stringify(geoJson))
-  }
-
-  updateSelectedGeometry(layer) {
-    const data = layer.toGeoJSON()
-    // store geoJSON in local storage
-    if (layer instanceof L.Circle) {
-      data.properties.radius = layer.getRadius()
-    }
-    this.saveSelectedGeometry(data)
-    let area = 0
-    if (layer instanceof L.Circle) {
-      const geometry = circleToPolygon(
-        data.geometry.coordinates,
-        layer.getRadius(),
-        this.defaultCircleToPolygonEdges
-      )
-      data.geometry = geometry
-      area = layer.getRadius() * Math.PI * Math.PI
-    } else if (layer instanceof L.Marker) {
-      area = 0
-    } else {
-      area = L.GeometryUtil.geodesicArea(layer.getLatLngs()[0])
-    }
-    this.$api().dataset.setGeometry({
-      geometry: data.geometry,
-      area,
-    })
   }
 
   @Watch('year')
@@ -353,14 +402,6 @@ class Map extends Vue {
     }
   }
 
-  getSavedGeometry() {
-    const skopeGeometry = this.$warehouse.get(this.wGeometryKey)
-    if (skopeGeometry) {
-      return JSON.parse(skopeGeometry)
-    }
-    return false
-  }
-
   fillTemplateYear(templateString) {
     const year = (this.year || this.maxTemporalRange).toString()
     const layer = fillTemplate(templateString, {
@@ -373,32 +414,35 @@ class Map extends Vue {
     return (leafletLayer.options.layers || '').startsWith('SKOPE')
   }
 
-  mapReady(map) {
-    const handler = (event) => {
-      console.log('handling layer change event ', { event })
-      const leafletLayer = event.layer
-      if (this.isSkopeLayer(leafletLayer)) {
-        const variable = _.find(
-          this.metadata.variables,
-          (v) => v.name === event.name
-        )
-        this.$api().dataset.setVariable(variable.id)
-        this.updateWmsLegend(map, leafletLayer.wmsParams.layers)
-        leafletLayer.bringToFront()
+  loadGeoJson(event) {
+    const file = event.target.files[0]
+    file.text().then((text) => {
+      console.log('received possible geojson to load: ', text)
+      try {
+        const geojson = JSON.parse(text)
+        this.$api().dataset.setGeoJSON(geojson)
+      } catch (error) {
+        console.error(error)
+        // FIXME: this should be a toast or other notification
+        alert("Sorry! We couldn't re-import this file: " + text)
       }
-    }
-    map.on('overlayadd', handler)
-    map.on('baselayerchange', handler)
-    this.addDrawToolbar(map)
+    })
   }
 
-  @Watch('geometry')
-  clearGeometry(geometry) {
-    if (geometry.type === 'None') {
-      console.log('clear selected geometry')
-      const L = this.$L
-      this.drawnItems.clearLayers()
-      this.$warehouse.remove(this.wGeometryKey)
+  selectGeoJsonFile() {
+    document.getElementById('loadGeoJsonFile').click()
+  }
+
+  exportSelectedGeometry(event) {
+    const geoJson = this.$api().dataset.geoJson
+    if (geoJson) {
+      console.log('exporting selected geoJson: ', { geoJson })
+      const convertedArea =
+        'text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(geoJson))
+      const button = document.getElementById('exportSelectedGeometry')
+      button.setAttribute('href', 'data:' + convertedArea)
+      // FIXME: generate a more unique name using the lat lngs or similar
+      button.setAttribute('download', `${this.metadata.id}.geojson`)
     }
   }
 }
