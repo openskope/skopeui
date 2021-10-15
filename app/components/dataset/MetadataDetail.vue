b30db50a987dad5c283804ffbacac718450c34b8
Allen Lee <allen.lee@asu.edu>
2021-10-14 22:01:59 -0700

    refactor: extract variable listing to component

--- app/components/dataset/MetadataDetail.vue
+++ app/components/dataset/MetadataDetail.vue
@@ -1,24 +1,6 @@
 <template>
   <v-card-text>
-    <v-list dense class="pa-0 ma-0">
-      <v-subheader class="text-h6 ma-0 pa-0">Variables</v-subheader>
-      <v-list-item v-for="(variable, index) in metadata.variables" :key="index">
-        <v-col class="pa-0 ma-0" cols="2">
-          <v-list-item-icon>
-            <v-chip small label dark color="primary">
-              {{ variable.class }}
-            </v-chip>
-          </v-list-item-icon>
-        </v-col>
-        <v-col class="pa-0 ma-0" cols="10">
-          <v-list-item-content>
-            <v-list-item-title>
-              {{ variable.name }}
-            </v-list-item-title>
-          </v-list-item-content>
-        </v-col>
-      </v-list-item>
-    </v-list>
+    <VariableList :variables="metadata.variables" />
     <div v-for="(label, attr) in metadataAttributes" :key="attr" class="py-0">
       <span class="text-subtitle-2">{{ label }}</span>
       <span v-html="$md.render(metadata[attr])"></span>
@@ -29,17 +11,17 @@
 <script>
 import Vue from "vue";
 import { Component, Prop } from "nuxt-property-decorator";
+import VariableList from "@/components/dataset/VariableList.vue";
 
 @Component({
   name: "MetadataDetail",
+  components: {
+    VariableList,
+  },
 })
 class MetadataDetail extends Vue {
   @Prop()
-  metadataId;
-
-  get metadata() {
-    return this.$api().metadata.find(this.metadataId);
-  }
+  metadata;
 
   get metadataAttributes() {
     return {
