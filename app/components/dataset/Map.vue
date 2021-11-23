5e26ae6ec18f88fb503989c1f4e55c2e7e004122
R. Kyle Bocinsky <bocinsky@users.noreply.github.com>
2021-11-23 14:03:44 -0700

    use caller instead of direct reference to opacity

--- app/components/dataset/Map.vue
+++ app/components/dataset/Map.vue
@@ -466,7 +466,7 @@ class Map extends Vue {
       VERSION: "1.0.0",
       FORMAT: "image/png",
       LAYER: this.wmsLayer,
-      ENV: "opacity:" + this.opacity/100,
+      ENV: "opacity:" + this.layerOpacity(),
       LEGEND_OPTIONS: "layout:vertical;dx:10",
     };
     const queryString = stringify(query);
